2307d466b8d29c34ba1c15e072e5f941e3e85594
doyeon <dansin0131@naver.com>
2018-01-22 12:20:12 +0900

    background 폴더변경(xml->layout)

---
+++ App_center/app/src/main/java/com/ourincheon/app_center/list.java
@@ -0,0 +1,14 @@
+package com.ourincheon.app_center;
+
+/**
+ * Created by doyeon on 2018-01-22.
+ */
+
+public class list {
+    int num;
+    String clubname; String introduce; String location;
+    String category; String image01;
+
+
+
+}
--- App_center/app/src/main/java/com/ourincheon/app_center/sports.java
+++ App_center/app/src/main/java/com/ourincheon/app_center/sports.java
@@ -4,14 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 /**
@@ -23,7 +20,7 @@ public class sports extends Activity{
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sports);
+        setContentView(R.layout.list_sports);
         TextView tv = (TextView)findViewById(R.id.place);
         String str = tv.getText().toString();
         SpannableStringBuilder ssb = new SpannableStringBuilder(str);
